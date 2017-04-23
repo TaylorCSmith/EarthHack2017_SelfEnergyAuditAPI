8907f3536cfe13238d7fe7cc95d0d27e56d9b763
Taylor Cody Smith <Taylor.Cody.Smith@gmail.com>
2017-04-22 19:33:31 -0500

    Creating base functional API

---
+++ EnergyAudit.Api/EnergyAudit.Api/Controllers/EnergyAuditController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergyAudit.Api.Controllers
+{
+    [Route("api/energyaudit")]
+    public class EnergyAuditController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("test")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
